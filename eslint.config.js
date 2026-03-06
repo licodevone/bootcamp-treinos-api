f25cac5ef6d30c145dc2b7ccfb1c5d5bae9c2b90
Luis E. S. Pinheiro <lepinheiro100@terra.com.br>
2026-03-06 17:53:55 -0300

    chore: refactor ESLint configuration and update package.json dependencies; implement Fastify server in index.ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
-import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
-    plugins: { js }, extends: ["js/recommended"], 
-    languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
   eslintConfigPrettier,
   {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-const teste = "";
+import 'dotenv/config';
 
+// Require the framework and instantiate it
 
+// ESM
+import Fastify from 'fastify';
 
+const fastify = Fastify({
+  logger: true,
+});
 
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' });
+})
+
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.PORT || '3000') });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+start();
